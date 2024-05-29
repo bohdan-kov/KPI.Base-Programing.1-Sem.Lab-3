82f0ba6c2bae6bf2cad7e347a1372568d7bbcf9b
Koval Bohdan <bohdankoval3012@gmail.com>
2024-05-29 18:57:09 +0300

    Add lab

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float sum = 0;
+    float point_1 = 0;
+    float point_2 = 0;
+
+    for (float x = 0; x < 1.02; x += 0.003) {   
+        cout << x << endl;
+        point_1 = 1 / cos(x);
+        point_2 = 3 - pow(x, 2);
+        sum += point_2 - point_1;
+    }
+
+    cout << sum << endl;
+}
